61cb1ba58f45ed252d8093170bfb5165644b5294
Vanessa Estrada <vestrada64@gmail.com>
2018-01-18 09:05:23 -0800

    Deploy app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root 'games#index'
     
     resources :games do
-      resources :comments, only: [:create, :destroy], shallow: true
+       resources :comments, only: [:create, :destroy, :edit, :update], shallow: true
     end
     resources :users, only: [:new, :create]
     resources :sessions, only: [:new, :create, :destroy]
